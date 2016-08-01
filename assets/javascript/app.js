3a9a06872b6d4f945b700a1b61f2d5f41eded52f
Brian Tom <briantom171@gmail.com>
2016-08-01 01:04:03 -0700

    second commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+var answers = [];
+var youranswers = [];
+var all = document.getElementsByTagName("input");
+
+var correctAnswer = 0;
+var wrongAnswer = 0;
+
+function fsubmit() {
+	
+	for (i = 0; i < all.length; i++) {
+		inputs = all[i].getAttribute("id");
+		if (inputs.indexOf("true")==0) {
+			inputs2 = all[i].getAttribute("value");
+			answers.push(inputs2);
+		}
+	}
+
+	for (i = 0; i < all.length; i++) {
+		inputs = all[i].getAttribute("id");
+		if (inputs.checked) {
+			inputs3 = all[i].getAttribute("value");
+			youranswers.push(inputs3);
+		}
+	}
+
+	if (checkAnswer.checked === true) {
+		correctAnswer++;
+		alert("Correct! Your score is " + correctAnswer + ".")
+	}
+	else {
+		wrongAnswer++;
+		if (wrongAnswer == 1) {
+			alert("Incorrect. You have guessed " + wrongAnswer + " wrong answer.")
+		}
+		else {
+			alert("Incorrect. You have guessed " + wrongAnswer + " wrong answers.")
+		}
+	}
+}
