032fbc5c556a815615db7d3a6f9a1707ee74d264
Brian Tom <briantom171@gmail.com>
2016-08-01 20:48:24 -0700

    fourth commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,42 @@ var all = document.getElementsByTagName("input");
 var correctAnswer = 0;
 var wrongAnswer = 0;
 
+var number = 30;
+var counter;
+
+// The run function sets an interval
+// that runs the decrement function once a second.
+// Notice how we name the interval "counter."
+function run(){
+	counter = setInterval(decrement, 1000);
+}
+
+function decrement(){
+	number--;
+    // Show the number in the #show-number tag.
+    $('#show-number').html('<h2>' + number + '</h2>');
+
+    // Once number hits zero...
+    if (number === 0){
+    // ...run the stop function.
+    	stop();
+    // Alert the user that time is up.
+    	alert('Time Up!')
+    }
+}
+
+function stop(){
+	// Clears our "counter" interval.
+	// We just pass the name of the interval
+	// to the clearInterval function.
+	clearInterval(counter);
+}
+
+run();
+
 function fsubmit() {
+
+	stop();
 	
 	for (i = 0; i < all.length; i++) {
 		inputs = all[i].getAttribute("id");
@@ -22,19 +57,22 @@ function fsubmit() {
 		}
 	}
 
-	for (i = 0; i < answers.length; i++) {
-		if (answers[i] == youranswers[i]) {
-			correctAnswer++;
-			alert("Correct! Your score is " + correctAnswer + ".")
-		}
-		else {
-			wrongAnswer++;
-			if (wrongAnswer == 1) {
-				alert("Incorrect. You have guessed " + wrongAnswer + " wrong answer.")
+	if (answers.length == youranswers.length){
+
+		for (i = 0; i < answers.length; i++) {
+			if (answers[i] == youranswers[i]) {
+				correctAnswer++;
 			}
 			else {
-				alert("Incorrect. You have guessed " + wrongAnswer + " wrong answers.")
+				wrongAnswer++;
 			}
 		}
 	}
+	else {
+		alert("You did not answer all the questions!");
+	}
+
+	var output = document.getElementById("game");	
+	var outputStr = "<h1>" + "Correct: " + correctAnswer + "</h1> " + "<h1>" + "Incorrect: " + wrongAnswer + "</h1>";
+	output.innerHTML = outputStr;
 }
